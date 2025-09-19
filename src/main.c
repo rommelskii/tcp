eb6b2b9a72ed57069ca6ce4245b5f24bfa4aa0cd
Rommel John Ronduen <rjhronduen@mymail.mapua.edu.ph>
2025-09-19 22:29:04 +0800

    added socket option for reusability

--- src/main.c
+++ src/main.c
@@ -24,6 +24,14 @@ int main() {
 		exit(EXIT_FAILURE);
 	}
 
+
+	//set socket for debug mode
+	int opt = 1;
+	if ( setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;
 	address.sin_port = htons(PORT);
